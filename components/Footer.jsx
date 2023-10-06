50a9c9377f8c6eddb7a9a7240a3aaf063f8003c2
onyango abbey <117769358+othinAbbey@users.noreply.github.com>
2023-10-06 17:08:31 +0300

    Modify footer style

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,9 +3,11 @@ import React from "react";
 function Footer() {
   return (
     <footer className="fixed bottom-0 w-full bg-gray-800 text-white p-4">
-      <div className="container mx-auto">
-        <h3 className="text-2xl font-semibold">Kanzu Banking</h3>
-        <p>&copy; {new Date().getFullYear()} All rights reserved</p>
+      <div className="container mx-auto grid place-items-center">
+        <div>
+          <h3 className="text-2xl font-semibold">Kanzu Banking</h3>
+          <p>&copy; {new Date().getFullYear()} All rights reserved</p>
+        </div>
       </div>
     </footer>
   );
